5920df673ac7242d581db2b55a1fbaab61dca1e5
aditya-thinkitive-1 <aditya.ranaware@thinkitive.com>
2025-06-03 19:30:39 +0530

    'Day2_react'

---
+++ React_Day2/src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,105 @@
+import "./ExpenseForm.css";
+import { useState } from "react";
+function ExpenseForm() {
+  //here we create state---
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState("");
+  const [date, setDate] = useState("");
+
+  //combine 3 states into one array
+  // const[Values,setUserInput]=useState({
+  //     title : '',
+  //     amount : '',
+  //     date : ''
+  // });
+  /*
+const titleHandler=(event)=>{
+    // setUserInput({
+    //     ...Values,
+    //     title : event.target.value,
+    // })
+    setTitle(event.target.value);
+}
+
+const amountHandler=(event)=>{
+    // setUserInput({
+    //     ...Values,
+    //     amount : event.target.value,
+    // })
+    setAmount(event.target.value)
+}
+
+const dateHandler=(event)=>{
+    // setUserInput((oldValues)=>{
+    //     return {...oldValues,
+    //     date : event.target.value};
+    // });
+    setDate(event.target.value)
+    console.log(title,amount,date);
+};
+*/
+  // we create one function called inputHandler which update all 3 attributes using identifier
+  function inputHandler(identifier, value) {
+    if (identifier === "title") {
+      setTitle(value);
+    } else if (identifier === "date") {
+      setDate(value);
+    } else {
+      setAmount(value);
+    }
+  }
+  //on Submiting form
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expressData = {
+      title: title,
+      amount: amount,
+      date: new Date(date),
+    };
+    console.log(expressData);
+    setAmount("");
+    setDate("");
+    setTitle("");
+    console.log(expressData);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input
+            type="text"
+            value={title}
+            onChange={(event) => inputHandler("title", event.target.value)}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            value={amount}
+            min="0.01"
+            step="0.01"
+            onChange={(event) => inputHandler("amount", event.target.value)}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            value={date}
+            min="2022-01-01"
+            max="2025-06-01"
+            onChange={(event) => inputHandler("date", event.target.value)}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+}
+export default ExpenseForm;
